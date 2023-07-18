5e8263aea891dcc34b52941c5e5e5f55920323b5
Ocramjr <mapmjunior@gmail.com>
2023-07-18 19:56:23 -0300

    Aula de For in, For of e Objetos

---
+++ index.js
@@ -0,0 +1,47 @@
+//For in e For of
+
+const nome = "Marco Antônio"
+
+for (let letrasNome of nome){
+    console.log(letrasNome)
+}
+
+
+for (let letrasNome in nome){
+    console.log(letrasNome)
+}
+
+// Objetos
+
+const carro = {
+    marca: 'fiat',
+    modelo: 'Sedan',
+    ano: 2023,
+    cor: 'vermelho',
+}
+
+console.log(carro.marca)
+
+for (const chave in carro) {
+    console.log(chave, carro[chave])
+}
+
+
+const aluno = {
+    nome: 'Marco Antônio',
+    matricula: 'FS21',
+    idade: 34
+}
+
+console.log (aluno.nome, aluno.matricula)
+
+
+const produtos = [
+    {nome: 'Camisa', valor: 35},
+    {nome: 'Calça', valor: 300},
+    {nome: 'Sapatos', valor: 65}
+]
+
+for (const nomes in produtos) {
+    console.log()
+}
